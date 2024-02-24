4927464ee3860de355ab0d998fe859e2d404dc05
Vishal <vbalagani8@gmail.com>
2024-02-23 23:42:52 -0500

    Average yearly rainfall per city, new dataset

--- coords.py
+++ coords.py
@@ -2,7 +2,7 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 
 # Load your spreadsheet
-df = pd.read_excel("cities.xlsx")
+df = pd.read_excel("FinalData.xlsx")
 
 # Initialize the geocoder (Nominatim from OpenStreetMap)
 geolocator = Nominatim(user_agent="mtfc muthu dogs")
@@ -10,6 +10,13 @@ geolocator = Nominatim(user_agent="mtfc muthu dogs")
 
 # Function to get the coordinates for each row and save the updated DataFrame
 def get_coordinates(index, row):
+    # Skip rows when errors occur
+    if index < 218:
+        return row
+
+    if row["City"] == "San Diego" or row["City"] == "San diego":
+        return row
+
     # get location from geolocator
     location = geolocator.geocode(f"{row['City']}, {row['State']}")
     # If the location is found, update the DataFrame
@@ -17,7 +24,7 @@ def get_coordinates(index, row):
         df.at[index, "Latitude"] = location.latitude
         df.at[index, "Longitude"] = location.longitude
         print(
-            f"Coordinates for {row['City']}, {row['State']} found: {location.latitude}, {location.longitude}"
+            f"Coordinates for Row {index}: {row['City']}, {row['State']} found: {location.latitude}, {location.longitude}"
         )
     # Save the updated DataFrame to an Excel file
     df.to_excel("cities_with_coordinates.xlsx", index=False)
--- weather.py
+++ weather.py
@@ -1,30 +1,74 @@
 import requests
 import pandas as pd
 import json
+from config import AZURE_API_KEY
 
 # Load your spreadsheet
-df = pd.read_excel("cities.xlsx")
+df = pd.read_excel("newData.xlsx")
 
-def get_coordinates(index, row):
+dates = [
+    "2023-01-01",
+    "2023-01-31",
+    "2023-02-01",
+    "2023-02-28",
+    "2023-03-01",
+    "2023-03-31",
+    "2023-04-01",
+    "2023-04-30",
+    "2023-05-01",
+    "2023-05-31",
+    "2023-06-01",
+    "2023-06-30",
+    "2023-07-01",
+    "2023-07-31",
+    "2023-08-01",
+    "2023-08-31",
+    "2023-09-01",
+    "2023-09-30",
+    "2023-10-01",
+    "2023-10-31",
+    "2023-11-01",
+    "2023-11-30",
+    "2023-12-01",
+    "2023-12-31",
+]
 
-    data = json.loads(response.text)
-    weatherdf = pd.DataFrame(data)
 
-    coordinates = f"{df.at[index, "Latitude"]}, {df.at[index, "Longitude"]}"
-    date = df.at[index, "DateAPI"]
-    azurePK = "dbmHvhR94mRMlP0MflDiAmtEW5zr0BjjDem5QXK0zho"
-    api_url = f"https://atlas.microsoft.com/weather/historical/actuals/daily/json?api-version=1.1&query={coordinates}&startDate={date}&endDate={date}&subscription-key={azurePK}"
+def get_precip(index, row):
+    if index < 133:
+        return row
 
-    response = requests.get(api_url)
-    response.raise_for_status()
+    rainfallTotal = 0
+    snowfallTotal = 0
+    coordinates = f"{df.at[index, 'lat']}, {df.at[index, 'lng']}"
 
-    df.at[index, "Precipitation"] = weatherdf['results'][0]['precipitation']['value']
-    df.at[index, "Snowfall"] = weatherdf['results'][0]['snowfall']['value']
+    for i in range(0, len(dates), 2):
+        startDate = dates[i]
+        endDate = dates[i + 1]
+        api_url = f"https://atlas.microsoft.com/weather/historical/actuals/daily/json?api-version=1.1&query={coordinates}&startDate={startDate}&endDate={endDate}&subscription-key={AZURE_API_KEY}"
+        response = requests.get(api_url)
+        response.raise_for_status()
+        data = json.loads(response.text)
+        weatherdf = pd.DataFrame(data)
+        for i in range(len(weatherdf["results"])):
+            rainfallTotal += weatherdf["results"][i]["precipitation"]["value"]
+            if weatherdf["results"][i].get("snowfall") is not None:
+                snowfallTotal += weatherdf["results"][i]["snowfall"]["value"]
+        print(
+            f"Weather for {row['city']}, {row['state_id']} from {startDate} to {endDate} found: {rainfallTotal} inches of precipitation"
+        )
+
+    df.at[index, "Average Precipitation"] = rainfallTotal / 365
+    df.at[index, "Average Snowfall"] = snowfallTotal / 365
+
+    print(
+        f"\nAverage precipitation for {row['city']}, {row['state_id']} is {rainfallTotal / 365} inches\n"
+    )
 
     # Save the updated DataFrame to an Excel file
-    df.to_excel("cities_with_weather.xlsx", index=False)
+    df.to_excel("scriptData.xlsx", index=False)
     return row
 
 
 # Apply the geocoding function and save after each row
-df.apply(lambda row: get_coordinates(row.name, row), axis=1)
+df.apply(lambda row: get_precip(row.name, row), axis=1)
